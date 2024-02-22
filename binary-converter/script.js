1d13f26fc7f621e43e0dbdbce5e128c0bf7ba729
aldrine <codingwithaldrine@gmail.com>
2024-02-22 23:58:10 +0800

    Implement recursive decimal to binary conversion up to step 73

--- binary-converter/script.js
+++ binary-converter/script.js
@@ -2,12 +2,22 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
+const decimalToBinary = input => {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
     alert('Please provide a decimal number');
     return;
   }
-  console.log(numberInput.value);
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = '';
 };
 
 convertBtn.addEventListener('click', checkUserInput);
